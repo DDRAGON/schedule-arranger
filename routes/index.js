6e149a3aa9a0aa35021acccab3f27bd0b4f63d91
DDRAGON <davide.0829@gmail.com>
2023-12-15 17:41:59 +0900

    render対応

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
+'use strict';
 const express = require('express');
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient({ log: [ 'query' ] });
+
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Tokyo');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  const title = '予定調整くん';
+  if (req.user) {
+    const schedules = await prisma.schedule.findMany({
+      where: { createdBy: parseInt(req.user.id)},
+      orderBy: { updatedAt: 'desc' }
+    });
+    schedules.forEach((schedule) => {
+      schedule.formattedUpdatedAt = dayjs(schedule.updatedAt).format('YYYY年MM月DD日 HH時mm分');
+    });
+    res.render('index', {
+      title: title,
+      user: req.user,
+      schedules: schedules
+    });
+  } else {
+    res.render('index', { title, user: req.user })
+  }
 });
 
 module.exports = router;
